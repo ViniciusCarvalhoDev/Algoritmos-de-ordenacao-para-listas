b34debff9f619fe39282db78aec65b5d9f2b70b6
Vinicius Carvalho <51410946+ViniciusCarvalhoDev@users.noreply.github.com>
2021-08-14 23:23:03 -0300

    nova-logica-acrescentada

--- ImplementandoAlgoritmos/ImplementandoAlgoritmos.cpp
+++ ImplementandoAlgoritmos/ImplementandoAlgoritmos.cpp
@@ -1,129 +1,81 @@
 #include <iostream>
+#include <iostream>
 #include <string>
 #include <cstdio>
 #include <cstdlib>
 #include "../../../../Desktop/tp/include/Fila.hpp"
+#include "../../../../Desktop/tp/src/Linha.cpp"
 
 //função que compara strings
 
-std::string compara(std::string n1, std::string n2) {
-    int aux = -1;
+bool compara(std::string n1, std::string n2) {
 
-    if (n1.length() > n2.length()) {
-        for (int i = 0; i < n2.length(); i++)
-        {
-            if (n2[i] != n1[i])
-            {
-                if (n2[i] < n1[i]) {
-                    aux = 2;
-                }
-                else {
-                    aux = 1;
-                }
-                break;
-            }
-        }
-    }
-    else if (n2.length() > n1.length()) {
-        for (int i = 0; i < n1.length(); i++)
-        {
-            if (n1[i] != n2[i])
-            {
-                if (n1[i] < n2[i])
-                {
-                    aux = 1;
-                }
-                else {
-                    aux = 2;
-                }
-                break;
-            }
-        }
-    }
-    else {
-        for (int i = 0; i < n1.length(); i++)
-        {
-            if (n1[i] != n2[i])
-            {
-                if (n1[i] < n2[i])
-                {
-                    aux = 1;
-                }
-                else {
-                    aux = 2;
-                }
+    int tamanho;
 
-            }
-            else {
-                aux = 0;
-            }
-        }
-    }
-
-    if (aux == 1)
-    {
-        return "A primeira vem antes";
-    }
-    else if (aux == 2) {
-        return "A segunda vem antes";
-    }
-    else if (aux == 0)
-    {
-        return "Sao iguais";
-    }
+    if (n1.size() < n2.size() == true)
+        tamanho = n1.size();
     else
+        tamanho = n2.size();
+
+    for (int i = 0; i < tamanho; i++)
     {
-        if (n1.length() > n2.length())
-        {
-            return "A segunda vem antes";
-        }
-        else {
-            return "A primeira vem antes";
-        }
+        if (n1[i] != n2[i] == true)
+            return n1[i] < n2[i];
     }
+
+    if (n2.size() > n1.size() == true)
+        return true;
+
+    return false;
 }
 
 //--- quickSort
 
-void mergeSort(Fila<std::string> lista, int inicio, int fim) {
-
-    if (inicio >= fim)
-    {
-        return;
-    }
-}
-
-void trocar(int i, int j, Fila<std::string> *lista)
+void trocar(int i, int j, Fila<Linha> *lista)
 {
-    Celula<std::string>* item1 = lista->RecuperaPonteiro(i);
-    Celula<std::string>* item2 = lista->RecuperaPonteiro(j);
+    Celula<Linha>* item1 = lista->RecuperaPonteiro(i);
+    Celula<Linha>* item2 = lista->RecuperaPonteiro(j);
+
     std::string aux = "";
+    int aux2;
 
-    aux = item1->dado;
-    item1->dado = item2->dado;
-    item2->dado = aux;
+    aux = item1->dado.nome;
+    aux2 = item1->dado.dados;
+
+    item1->dado.nome = item2->dado.nome;
+    item1->dado.dados = item2->dado.dados;
+
+    item2->dado.nome = aux;
+    item2->dado.dados = aux2;
 }
 
-void trocarInt(int i, int j, Fila<int>* lista)
+void trocarInt(int i, int j, Fila<Linha>* lista)
 {
-    Celula<int>* item1 = lista->RecuperaPonteiro(i);
-    Celula<int>* item2 = lista->RecuperaPonteiro(j);
+    Celula<Linha>* item1 = lista->RecuperaPonteiro(i);
+    Celula<Linha>* item2 = lista->RecuperaPonteiro(j);
+
     int aux;
+    std::string aux2;
 
-    aux = item1->dado;
-    item1->dado = item2->dado;
-    item2->dado = aux;
+    aux = item1->dado.dados;
+    aux2 = item1->dado.nome;
+
+    item1->dado.dados = item2->dado.dados;
+    item1->dado.nome = item2->dado.nome;
+
+    item2->dado.dados = aux;
+    item2->dado.nome = aux2;
 }
 
-int partition(Fila<std::string> *lista, int low, int high)
+int partition(Fila<Linha> *lista, int low, int high)
 {
-    std::string pivot = lista->Recupera(high); 
+    std::string pivot = lista->RecuperaPonteiro(high)->dado.nome; 
     int i = (low - 1);  
 
     for (int j = low; j <= high - 1; j++)
     {
-        
-        if (compara(lista->Recupera(j), pivot) == "A primeira vem antes")
+        Linha aux = lista->RecuperaPonteiro(j)->dado;
+        if (compara(aux.nome, pivot) == true)
         {
             i++;    
             trocar(i,j, lista);
@@ -134,7 +86,7 @@ int partition(Fila<std::string> *lista, int low, int high)
 }
 
 
-void quickSort(Fila<std::string> *lista, int low, int high)
+void quickSort(Fila<Linha> *lista, int low, int high)
 {
     if (low < high)
     {
@@ -146,18 +98,25 @@ void quickSort(Fila<std::string> *lista, int low, int high)
     }
 }
 
-void merge(Fila<std::string>* lista, int const left, int const mid, int const right)
+void merge(Fila<Linha>* lista, int const left, int const mid, int const right)
 {
     int const subArrayOne = mid - left + 1;
     int const subArrayTwo = right - mid;
 
     Fila<std::string> lista1;
-    Fila<std::string> lista2;
+    Fila<int> lista1Int;
 
-    for (auto i = 0; i < subArrayOne; i++)
-        lista1.Enfileirar(lista->Recupera(left + i));
-    for (auto j = 0; j < subArrayTwo; j++)
-        lista2.Enfileirar(lista->Recupera(mid + 1 + j));
+    Fila<std::string> lista2;
+    Fila<int> lista2Int;
+
+    for (auto i = 0; i < subArrayOne; i++) {
+        lista1.Enfileirar(lista->RecuperaPonteiro(left + i)->dado.nome);
+        lista1Int.Enfileirar(lista->RecuperaPonteiro(left + i)->dado.dados);
+    }
+    for (auto j = 0; j < subArrayTwo; j++){
+        lista2.Enfileirar(lista->RecuperaPonteiro(mid + 1 + j)->dado.nome);
+        lista2Int.Enfileirar(lista->RecuperaPonteiro(mid + 1 +j)->dado.dados);
+    }
 
     auto indexOfSubArrayOne = 0,
         indexOfSubArrayTwo = 0; 
@@ -165,31 +124,35 @@ void merge(Fila<std::string>* lista, int const left, int const mid, int const ri
 
 
     while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo) {
-        if (compara(lista1.Recupera(indexOfSubArrayOne), lista2.Recupera(indexOfSubArrayTwo)) == "A primeira vem antes") {
-            lista->RecuperaPonteiro(indexOfMergedArray)->dado = lista1.Recupera(indexOfSubArrayOne);
+        if (compara(lista1.Recupera(indexOfSubArrayOne), lista2.Recupera(indexOfSubArrayTwo)) == true) {
+            lista->RecuperaPonteiro(indexOfMergedArray)->dado.nome = lista1.Recupera(indexOfSubArrayOne);
+            lista->RecuperaPonteiro(indexOfMergedArray)->dado.dados = lista1Int.Recupera(indexOfSubArrayOne);
             indexOfSubArrayOne++;
         }
         else {
-            lista->RecuperaPonteiro(indexOfMergedArray)->dado = lista2.Recupera(indexOfSubArrayTwo);
+            lista->RecuperaPonteiro(indexOfMergedArray)->dado.nome = lista2.Recupera(indexOfSubArrayTwo);
+            lista->RecuperaPonteiro(indexOfMergedArray)->dado.dados = lista2Int.Recupera(indexOfSubArrayTwo);
             indexOfSubArrayTwo++;
         }
         indexOfMergedArray++;
     }
 
     while (indexOfSubArrayOne < subArrayOne) {
-        lista->RecuperaPonteiro(indexOfMergedArray)->dado = lista1.Recupera(indexOfSubArrayOne);
+        lista->RecuperaPonteiro(indexOfMergedArray)->dado.nome = lista1.Recupera(indexOfSubArrayOne);
+        lista->RecuperaPonteiro(indexOfMergedArray)->dado.dados = lista1Int.Recupera(indexOfSubArrayOne);
         indexOfSubArrayOne++;
         indexOfMergedArray++;
     }
 
     while (indexOfSubArrayTwo < subArrayTwo) {
-        lista->RecuperaPonteiro(indexOfMergedArray)->dado = lista2.Recupera(indexOfSubArrayTwo);
+        lista->RecuperaPonteiro(indexOfMergedArray)->dado.nome = lista2.Recupera(indexOfSubArrayTwo);
+        lista->RecuperaPonteiro(indexOfMergedArray)->dado.dados = lista2Int.Recupera(indexOfSubArrayTwo);
         indexOfSubArrayTwo++;
         indexOfMergedArray++;
     }
 }
 
-void mergeSort(Fila<std::string>* lista, int const begin, int const end)
+void mergeSort(Fila<Linha>* lista, int const begin, int const end)
 {
     if (begin >= end) {
         return;
@@ -204,16 +167,16 @@ void mergeSort(Fila<std::string>* lista, int const begin, int const end)
 
 //heapSort
 
-void heapify(Fila<int>* lista, int n, int i)
+void heapify(Fila<Linha>* lista, int n, int i)
 {
     int largest = i; 
     int l = 2 * i + 1; 
     int r = 2 * i + 2; 
 
-    if (l < n && lista->Recupera(l) > lista->Recupera(largest))
+    if (l < n && lista->RecuperaPonteiro(l)->dado.dados > lista->RecuperaPonteiro(largest)->dado.dados)
         largest = l;
 
-    if (r < n && lista->Recupera(r) > lista->Recupera(largest))
+    if (r < n && lista->RecuperaPonteiro(r)->dado.dados > lista->RecuperaPonteiro(largest)->dado.dados)
         largest = r;
 
     if (largest != i) {
@@ -223,7 +186,7 @@ void heapify(Fila<int>* lista, int n, int i)
 }
 
 
-void heapSort(Fila<int> *lista, int n)
+void heapSort(Fila<Linha> *lista, int n)
 {
     // Build heap (rearrange array)
     for (int i = n / 2 - 1; i >= 0; i--)
@@ -242,38 +205,38 @@ void heapSort(Fila<int> *lista, int n)
 
 //hadix 
 
-int getMax(Fila<int>* lista, int n)
+int getMax(Fila<Linha>* lista, int n)
 {
-    int mx = lista->Recupera(0);
+    int mx = lista->RecuperaPonteiro(0)->dado.dados;
     for (int i = 1; i < n; i++)
-        if (lista->Recupera(i) > mx)
-            mx = lista->Recupera(i);
+        if (lista->RecuperaPonteiro(i)->dado.dados > mx)
+            mx = lista->RecuperaPonteiro(i)->dado.dados;
     return mx;
 }
 
 
-void countSort(Fila<int>* lista, int n, int exp)
+void countSort(Fila<Linha>* lista, int n, int exp)
 {
     int *output = new int[n];
     int i, count[10] = { 0 };
 
     for (i = 0; i < n; i++)
-        count[(lista->Recupera(i) / exp) % 10]++;
+        count[(lista->RecuperaPonteiro(i)->dado.dados / exp) % 10]++;
 
     for (i = 1; i < 10; i++)
         count[i] += count[i - 1];
 
     for (i = n - 1; i >= 0; i--) {
-        output[count[(lista->Recupera(i) / exp) % 10] - 1] = lista->Recupera(i);
-        count[(lista->Recupera(i) / exp) % 10]--;
+        output[count[(lista->RecuperaPonteiro(i)->dado.dados / exp) % 10] - 1] = lista->RecuperaPonteiro(i)->dado.dados;
+        count[(lista->RecuperaPonteiro(i)->dado.dados / exp) % 10]--;
     }
 
     for (i = 0; i < n; i++)
-        lista->RecuperaPonteiro(i)->dado = output[i];
+        lista->RecuperaPonteiro(i)->dado.dados = output[i];
 }
 
 // Radix Sort
-void radixsort(Fila<int>* lista, int n)
+void radixsort(Fila<Linha>* lista, int n)
 {
     int m = getMax(lista, n);
 
@@ -284,16 +247,16 @@ void radixsort(Fila<int>* lista, int n)
 
 int main()
 {
-    std::string n1 = "Lily 01100100";
-    std::string n2 = "Leana 10011100";
-    std::string n3 = "Rosetta 00000000";
-    std::string n4 = "Floy 01011100";
-    std::string n5 = "Eve 01000010";
-    std::string n6 = "Norah 11001101";
-    std::string n7 = "Walter 01001000";
-    std::string n8 = "Jennie 11011111";
-    std::string n9 = "Leone 00101110";
-    std::string n10 = "Carlotta 11101001";
+    std::string n1 = "Lily";
+    std::string n2 = "Leana";
+    std::string n3 = "Rosetta";
+    std::string n4 = "Floy";
+    std::string n5 = "Eve";
+    std::string n6 = "Norah";
+    std::string n7 = "Walter";
+    std::string n8 = "Jennie";
+    std::string n9 = "Leone";
+    std::string n10 = "Carlotta";
 
     int n11 = std::stoi("01100100");
     int n21 = std::stoi("10011100");
@@ -306,11 +269,10 @@ int main()
     int n91 = std::stoi("00101110");
     int n101 = std::stoi("11101001");
 
-    Celula<std::string>* itemAtual = nullptr;
-    
-
     Fila<std::string> listaNomes;
     Fila<int> listaInteiros;
+
+    Fila<Linha> linhas;
 
     listaNomes.Enfileirar(n1);
     listaNomes.Enfileirar(n2);
@@ -332,14 +294,29 @@ int main()
     listaInteiros.Enfileirar(n71);
     listaInteiros.Enfileirar(n81);
     listaInteiros.Enfileirar(n91);
-    listaInteiros.Enfileirar(n11);
+    listaInteiros.Enfileirar(n101);
 
-    //quickSort(&listaNomes,0,listaNomes.Tamanho()-1);
-    //mergeSort(&listaNomes, 0, listaNomes.Tamanho()-1);
-    //heapSort(&listaInteiros, listaNomes.Tamanho());
-    radixsort(&listaInteiros, listaNomes.Tamanho());
-    listaInteiros.Imprimir();
-    listaNomes.Imprimir();
+
+    for (int i = 0; i < listaInteiros.Tamanho(); i++)
+    {
+        Linha aux = Linha(listaNomes.Recupera(i), listaInteiros.Recupera(i));
+        linhas.Enfileirar(aux);
+    }
+
+    //radixsort(&linhas, listaNomes.Tamanho());
+    heapSort(&linhas, 10);
+    mergeSort(&linhas, 0, listaNomes.Tamanho()-1);
+    
+    //radixsort(&linhas, listaNomes.Tamanho());
+    //quickSort(&linhas,0,listaNomes.Tamanho()-1);
+
+    //linhas.Imprimir();
+
+    for (int i = 0; i < linhas.Tamanho(); i++)
+    {
+        Linha dados = linhas.RecuperaPonteiro(i)->dado;
+        std::cout << dados.nome << " " << dados.dados << std::endl;
+    }
 
     return 0;
 }
